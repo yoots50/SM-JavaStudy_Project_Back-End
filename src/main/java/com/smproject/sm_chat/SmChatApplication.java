5e2a5d4fb0e88442752b62cdda24590c79615f21
yoots50 <202210947@sangmyung.kr>
2025-10-28 12:41:52 +0900

    Initial project using Spring Initializr

---
+++ src/main/java/com/smproject/sm_chat/SmChatApplication.java
@@ -0,0 +1,13 @@
+package com.smproject.sm_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmChatApplication.class, args);
+	}
+
+}
